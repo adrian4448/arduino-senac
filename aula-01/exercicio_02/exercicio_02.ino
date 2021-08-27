e2a452ce70fbe24b886eb4ee8d51e3a13fa60a6c
adrian4448 <adrian.morais4448@gmail.com>
2021-08-26 21:53:34 -0300

    Aula 01 - Semaforo com LEDS

---
+++ aula-01/exercicio_02/exercicio_02.ino
@@ -0,0 +1,41 @@
+int vermelho = 13;
+int amarelo = 12;
+int verde = 11;
+int pare = 10;
+int prossiga = 9;
+
+void setup() {
+  pinMode(vermelho, OUTPUT);
+  pinMode(amarelo, OUTPUT);
+  pinMode(verde, OUTPUT);
+  pinMode(pare, OUTPUT);
+  pinMode(prossiga, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(prossiga, HIGH);
+  digitalWrite(vermelho, HIGH);
+  delay(3000);
+  digitalWrite(prossiga, LOW);
+  piscar(pare);
+  delay(200);
+  piscar(pare);
+  delay(200);
+  piscar(pare);
+  delay(200);
+  digitalWrite(vermelho, LOW);
+  digitalWrite(pare, HIGH);
+  digitalWrite(amarelo, HIGH);
+  delay(3000);
+  digitalWrite(amarelo, LOW);
+  digitalWrite(verde, HIGH);
+  delay(3000);
+  digitalWrite(verde, LOW);
+  digitalWrite(pare, LOW);
+}
+
+void piscar(int porta) {
+  digitalWrite(porta, HIGH);
+  delay(1000);
+  digitalWrite(porta, LOW);
+}
